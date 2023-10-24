0736b51d0f734350f8573b549a0ab00bb627f09a
Adam <duonggiakhanhb@gmail.com>
2023-10-24 10:27:33 +0700

    update spark

--- pipelines/kafka_stream.py
+++ pipelines/kafka_stream.py
@@ -2,20 +2,6 @@ import json
 # from kafka import KafkaProducer
 import time
 
-def get_motogp_page(year="2022", session="ina", country="spr"):
-    import requests
-
-    
-    res = requests.get(f"https://www.motogp.com/en/gp-results/{year}/{country}/motogp/{session}/classification", timeout=10)
-    return res.text
-
-def get_motogp_data():
-    from bs4 import BeautifulSoup
-
-    soup = BeautifulSoup(get_motogp_page(), 'html.parser')
-    # print(soup.prettify())
-    table = soup.find(attrs={"id": "main-content"})
-    print(table)
 
 # https://api.motogp.pulselive.com/motogp/v1/results/seasons
 # return all seasons
@@ -118,23 +104,31 @@ def get_motogp_result_session_api(session):
 # print(get_motogp_session_api("bfd8a08c-cbb4-413a-a210-6d34774ea4c5", "e8c110ad-64aa-4e8e-8a86-f2f152f6a942"))
 # print(get_motogp_all_season_api())
 
-import random
+# import random
 
-# choose 1 random season from get_motogp_all_season_api()
-season_id = random.choice(get_motogp_all_season_api())["id"]
-print(season_id)
+# # choose 1 random season from get_motogp_all_season_api()
+# season_gp = random.choice(get_motogp_all_season_api())
+# print(season_gp.keys())
+# season_id = season_gp["id"]
+# print(season_id)
 
-# choose 1 random event from get_motogp_event_api(session_id)
-event_id = random.choice(get_motogp_event_api(season_id))["id"]
-print(event_id)
+# # choose 1 random event from get_motogp_event_api(session_id)
+# event_gp = random.choice(get_motogp_event_api(season_id))
+# print(event_gp.keys())
+# event_id = event_gp["id"]
+# print(event_id)
 
-# choose 1 random category from get_motogp_category_api(event_id)
-category_id = random.choice(get_motogp_category_api(event_id))["id"]
-print(category_id)
+# # choose 1 random category from get_motogp_category_api(event_id)
+# category_gp = random.choice(get_motogp_category_api(event_id))
+# print(category_gp.keys())
+# category_id = category_gp["id"]
+# print(category_id)
 
-# choose 1 random session from get_motogp_session_api(event_id, category_id)
-session_id = random.choice(get_motogp_session_api(event_id, category_id))["id"]
-print(session_id)
+# # choose 1 random session from get_motogp_session_api(event_id, category_id)
+# session_gp = random.choice(get_motogp_session_api(event_id, category_id))
+# print(session_gp.keys())
+# session_id = session_gp["id"]
+# print(session_id)
 
-# print result of session
-print(get_motogp_result_session_api(session_id)) 
+# # print result of session
+# print(get_motogp_result_session_api(session_id)) 
